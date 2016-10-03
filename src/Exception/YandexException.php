e1235ee254ae0a1d725522de7cdea176b3a5da46
Slava Basko <basko.slava@gmail.com>
2016-10-03 15:44:22 +0300

    return host_id on conflict

--- src/Exception/YandexException.php
+++ src/Exception/YandexException.php
@@ -8,5 +8,29 @@ namespace Yandex\Exception;
 
 class YandexException extends \Exception
 {
+    /**
+     * @var array
+     */
+    protected $extra = [];
 
+    /**
+     * YandexException constructor.
+     * @param string $message
+     * @param int $code
+     * @param \Exception|null $previous
+     * @param array $extra
+     */
+    public function __construct($message = '', $code = 0, \Exception $previous = null, $extra = [])
+    {
+        parent::__construct($message, $code, $previous);
+        $this->extra = $extra;
+    }
+
+    /**
+     * @return array
+     */
+    public function getExtra()
+    {
+        return $this->extra;
+    }
 }
